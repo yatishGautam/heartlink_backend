8b5a9e751a384ed72ce7584252e64c8a744a85a9
yatish <ydipankar@gmail.com>
2024-08-18 18:15:27 -0300

    changes

---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+import express from "express";
+const router = express.Router();
+
+router.post("/signup", (req, res) => {
+	//singup method
+	console.log("singup");
+	res.send("");
+});
+
+router.post("/singin", (req, resss) => {
+	//singin method
+	res.send("");
+});
+
+router.post("./logout", (req, res) => {
+	//logout method
+	res.send();
+});
+
+module.exports = router;
