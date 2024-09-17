f87efc5ba3133b576a6120916ff44154cde3ceff
yatish <ydipankar@gmail.com>
2024-09-16 22:23:43 -0300

    added cors for local dev

--- app.js
+++ app.js
@@ -11,9 +11,15 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var authRouter = require("./routes/auth");
 var globalErrorHandler = require("./Middlewares/GlobalErrorHandler");
-
+const cors = require("cors");
 var app = express();
-
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+	})
+);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
